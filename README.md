ceb5c309fc3e3b96276a3bf54c5fe6be8a7c3b27
Jkrox <jkrox@protonmail.com>
2024-02-18 21:13:56 -0500

    Add installation instructions for dependencies and setup script

--- README.md
+++ README.md
@@ -9,3 +9,25 @@ cd
 sudo apt update && sudo apt upgrade
 sudo apt install wget git 
 ```
+
+Now, go to `assets/` folder in order to install some dependencies, please follow the next steps: 
+
+```sh
+# I'm supposing that you have already cloned the project 
+cd wsl-dotfiles/assets/
+sudo dpkg -i bat-musl_0.23.0_amd64.deb
+sudo dpkg -i lsd-musl_1.0.0_amd64.deb
+```
+With this two packages your output will look fancier and readable.
+
+> 📝 However, with these packages you may have to use a enhanced font which supports as many icons as possible.
+> Use fonts from [Nerd Font](https://www.nerdfonts.com/font-downloads), in my case, I have been using [this one](https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/CascadiaCode.zip)
+-------------------------
+*Done that, you have to execute the `dotfile.sh` file.*
+Follow the next  steps:
+```sh
+chmod +x dotfiles.sh
+./dotfiles.sh
+```
+
+That's it, you got set up your dev enviroment 🐱‍💻.
--- dotfiles.sh
+++ dotfiles.sh
@@ -1,3 +1,30 @@
+#!/bin/bash
+
 sudo apt install zsh -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+
+# Setting up .zshrc and .p10k.zsh
+file1_in_home=~/.zshrc
+file2_in_home=~/.p10k.zsh
+folder_config=config
+file1_in_config=$folder_config/.zshrc
+file2_in_config=$folder_config/.p10k.zsh
+
+# Verificar si los archivos en ~/ existen
+if [ -e "$file1_in_home" ] && [ -e "$file2_in_home" ]; then
+    # Copiar y reemplazar archivo1
+    cp -f "$file1_in_home" "$file1_in_config"
+    echo "Se ha reemplazado $file1_in_config con $file1_in_home"
+
+    # Copiar y reemplazar archivo2
+    cp -f "$file2_in_home" "$file2_in_config"
+    echo "Se ha reemplazado $file2_in_config con $file2_in_home"
+
+    echo "Operación completada."
+else
+    echo "Error: Los archivos en ~/ no existen."
+fi
